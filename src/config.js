ba2299e8586269883e62c1cdea251e40613e7882
idleberg <git@idleberg.com>
2020-12-06 03:14:33 +0100

    format

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const configSchema = {
     description: 'Specify a custom path to the `sassc` binary',
     type: 'string',
     default: 'sassc',
-    order: 0
+    order: 0,
   },
   customArguments: {
     title: 'Custom Arguments',
@@ -14,7 +14,7 @@ export const configSchema = {
       'Specify your preferred arguments for `sassc`, supports [replacement](https://github.com/noseglid/atom-build#replacement) placeholders',
     type: 'string',
     default: '--style compressed {FILE_ACTIVE} {FILE_ACTIVE_NAME_BASE}.min.css',
-    order: 1
+    order: 1,
   },
   manageDependencies: {
     title: 'Manage Dependencies',
@@ -22,7 +22,7 @@ export const configSchema = {
       'When enabled, third-party dependencies will be installed automatically',
     type: 'boolean',
     default: true,
-    order: 2
+    order: 2,
   },
   alwaysEligible: {
     title: 'Always Eligible',
@@ -30,8 +30,8 @@ export const configSchema = {
       'The build provider will be available in your project, even when not eligible',
     type: 'boolean',
     default: false,
-    order: 3
-  }
+    order: 3,
+  },
 };
 
 export function getConfig(key) {
